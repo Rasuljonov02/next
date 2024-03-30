bb8762ce0d151ea338df863356278e4e30ee559f
Rasuljonov02 <muhammadjonrasuljono4202@gmail.com>
2024-03-30 16:17:30 +0500

    gsngjr

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -15,7 +15,7 @@ export const skillsData = [
 	"Firebase",
 	"MaterialUI",
 	"Angular",
-  "Ubuntu",
+
 ];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
