6b075ead9d549bd020e76e3f847a04e0ad9579ef
muhamamd <muhammadjonrasuljonov4202@gmail.com>
2024-05-11 18:50:53 +0500

    nuxt qoshildi

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -118,10 +118,11 @@ function HeroSection() {
 							<div className="ml-4 lg:ml-8 mr-2">
 								<span className=" text-white">skills:</span>
 								<span className="text-gray-400">{`['`}</span>
-                
 								<span className="text-amber-300">React</span>
 								<span className="text-gray-400">{"', '"}</span>
 								<span className="text-amber-300">NextJS</span>
+								<span className="text-gray-400">{"', '"}</span>
+								<span className="text-amber-300">NuxtJS</span>
 								<span className="text-gray-400">{"', '"}</span>
 								<span className="text-amber-300">Redux</span>
 								<span className="text-gray-400">{"', '"}</span>
